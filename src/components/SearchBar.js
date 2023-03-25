d9c6aaff80014360854e73e3bed53fedb2cb0342
Lucas-De-Lima <lucasramosdelima@gmail.com>
2023-03-25 12:00:52 -0300

    fix: corrigindo diretório "/src"

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './SearchBar.css';
 
 export default function SearchBar() {
   return (
@@ -9,38 +10,46 @@ export default function SearchBar() {
           name="query"
           id="query"
           data-testid="search-input"
+          className="search-input"
+          autoComplete="off"
         />
       </label>
-      <div>
+      <div className="radio-search">
         <label htmlFor="ingredient">
-          Ingredient
           <input
             type="radio"
             name="ingredient"
             id="ingredient"
             data-testid="ingredient-search-radio"
           />
+          Ingredient
         </label>
         <label htmlFor="name">
-          Recipe name
           <input
             type="radio"
             name="name"
             id="name"
             data-testid="name-search-radio"
           />
+          Recipe name
         </label>
         <label htmlFor="first-letter">
-          First letter
           <input
             type="radio"
             name="first-letter"
             id="first-letter"
             data-testid="first-letter-search-radio"
           />
+          First letter
         </label>
       </div>
-      <button type="button" data-testid="exec-search-btn">Search</button>
+      <button
+        type="button"
+        data-testid="exec-search-btn"
+        className="search-btn"
+      >
+        Search
+      </button>
     </form>
   );
 }
--- src/pages/RecipeInProgress.js
+++ src/pages/RecipeInProgress.js
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
-import DetailHeader from '../components/DetailHeader';
-import IngredientList from '../components/IngredientList';
 import { cocktailDetails, mealDetails } from '../components/services/dataFetchApi';
+import './RecipeInProgress.css';
 
 function RecipeInProgress() {
   const { id } = useParams();
   const history = useHistory();
   const [recipe, setRecipe] = useState([]);
-  const { pathname } = history.location;
 
   const infoItem = async () => {
+    const { pathname } = history.location;
     let recipeInfo = recipe;
     if (pathname.includes('/meals')) {
       const getMeal = await mealDetails(id);
@@ -74,25 +73,27 @@ function RecipeInProgress() {
     return recipeInfo;
   };
 
-  const capitalize = (word) => word[0].toUpperCase() + word.substring(1, word.length - 1);
-  const rawPath = pathname.split('/');
-  const type = capitalize(rawPath[1]);
-  const path = rawPath.slice(0, (rawPath.length - 1)).join('/');
-
   return (
     <div>
       {recipe.length > 0
         && (
           <>
-            <DetailHeader recipe={ recipe[0] } type={ type } path={ path } />
+            <h1 data-testid="recipe-title">
+              {`Recipe: ${defineRecipe().recipeTitle}`}
+            </h1>
+            <img
+              data-testid="recipe-photo"
+              src={ defineRecipe().recipeImage }
+              alt={ defineRecipe().recipeTitle }
+              width="300"
+            />
+            <h2 data-testid="recipe-category">
+              { `Category: ${defineRecipe().recipeCategory}`}
+            </h2>
+
             <p data-testid="instructions">
               { `Instructions: ${defineRecipe().recipeInstructions}`}
             </p>
-            <IngredientList
-              ingredients={ defineRecipe().recipeIngredients }
-              measures={ defineRecipe().recipeMeasures }
-              inProgress
-            />
             <iframe
               data-testid="video"
               src={ defineRecipe().recipeVideo }
@@ -105,6 +106,19 @@ function RecipeInProgress() {
                 type="button"
               >
                 Terminar Receita
+              </button>
+              <button
+                data-testid="share-btn"
+                type="button"
+              >
+                Compartilhar
+              </button>
+
+              <button
+                data-testid="favorite-btn"
+                type="button"
+              >
+                Favoritar
               </button>
             </div>
           </>
